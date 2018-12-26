c2fb38c439b62d74da588b5b6d8a3fa1bced1452
Mingjie Xu <e0321098@u.nus.edu>
2018-12-26 10:55:22 +0800

    test1

--- app/src/main/java/books/com/bookstore/CategoryActivity.java
+++ app/src/main/java/books/com/bookstore/CategoryActivity.java
@@ -9,5 +9,6 @@ public class CategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+        //test1
     }
 }
